3d3333326ef97f44bb417d0257a04bc98be05a18
tnakamura0314 <Reggae.a0215@gmail.com>
2021-09-02 15:55:57 +0900

    ShowOrderConfirmationCOntrollerにOrderFormの@ModelAttributeを追加

--- src/main/java/jp/co/example/ecommerce_c/controller/ShowOrderConfirmationController.java
+++ src/main/java/jp/co/example/ecommerce_c/controller/ShowOrderConfirmationController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.example.ecommerce_c.domain.Order;
+import jp.co.example.ecommerce_c.form.OrderForm;
 import jp.co.example.ecommerce_c.service.ShowOrderConfirmationService;
 
 @Controller
@@ -16,6 +18,11 @@ public class ShowOrderConfirmationController {
 	
 	@Autowired
 	private ShowOrderConfirmationService service;
+	
+	@ModelAttribute
+	public OrderForm setUpForm() {
+		return new OrderForm();
+	}
 	
 	
 	@RequestMapping("/showOrderConfirmation")
--- src/main/java/jp/co/example/ecommerce_c/repository/OrderRepository.java
+++ src/main/java/jp/co/example/ecommerce_c/repository/OrderRepository.java
@@ -5,9 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
-
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
