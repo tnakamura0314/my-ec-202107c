ec7eef140b98d6b9fb709b54e0d0c32f9ff4d583
tnakamura0314 <Reggae.a0215@gmail.com>
2021-09-03 10:13:04 +0900

    各クラスのメソッドの修正

--- src/main/java/jp/co/example/ecommerce_c/controller/OrderHistoryController.java
+++ src/main/java/jp/co/example/ecommerce_c/controller/OrderHistoryController.java
@@ -1,9 +1,13 @@
 package jp.co.example.ecommerce_c.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.ecommerce_c.domain.Order;
 import jp.co.example.ecommerce_c.service.OrderHistoryService;
 
 /**
@@ -19,8 +23,23 @@ public class OrderHistoryController {
 	@Autowired
 	private OrderHistoryService service;
 	
+	/**
+	 * 注文履歴情報を表示する.
+	 * 
+	 * @param model requestスコープ
+	 * @return　注文履歴情報(1件もなければメッセージ)
+	 */
 	@RequestMapping("")
-	public String showHistory() {
+	public String showHistory(Model model) {
+		
+		//後ほど実装します
+//		List<Order> orderHistoryList = service.
+//		
+//		if(orderHistoryList == null) {
+//			model.addAttribute("errorMessage", "検索結果が1件もありませんでした");
+//			return "/order_history";
+//		}
+		
 		return "/order_history";
 	}
 
--- src/main/java/jp/co/example/ecommerce_c/service/OrderHistoryService.java
+++ src/main/java/jp/co/example/ecommerce_c/service/OrderHistoryService.java
@@ -1,8 +1,11 @@
 package jp.co.example.ecommerce_c.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jp.co.example.ecommerce_c.domain.Order;
 import jp.co.example.ecommerce_c.repository.OrderRepository;
 
 /**
@@ -16,5 +19,20 @@ public class OrderHistoryService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	
+	//後ほど実装します
+	/**
+	 * 注文履歴情報を取得するメソッド.
+	 * 
+	 * @return
+	 */
+//	public List<Order> orderHistory(){
+//		
+//		
+//		List<Order> orderHistoryList = repository.
+////		
+//		return orderHistoryList;
+//	}
 
 }
